8f9f0a0f719eb46395443a76fcec8a2f8865fe2b
ilyaf f <ilyaf@appveyor.com>
2017-11-03 19:28:04 -0700

    add xnunit tests

---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(true);
+        }
+    }
+}
