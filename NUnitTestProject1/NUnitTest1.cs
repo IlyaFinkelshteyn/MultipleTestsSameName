6111957b3afead22041fe2d9710ea640133d8ebd
ilyaf f <ilyaf@appveyor.com>
2017-11-03 19:11:46 -0700

    add nunit tests

---
+++ NUnitTestProject1/NUnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    //[TestFixture]
+    public class NUnitTest1
+    {
+        //[Test]
+        public void ClassicNunitTestMethod()
+        {
+            Assert.IsFalse(false);            
+        }
+    }
+}
